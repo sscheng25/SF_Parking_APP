e0118136555869a781734b7b352f52d5bb8dbb5e
Sisun Cheng <997723678@qq.com>
2022-04-07 19:51:27 -0400

    04/07 update

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let map = L.map('map').setView([37.7707, -122.4378], 12);
+
+L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+}).addTo(map);
