3d5874b6a489f551a98a51f815fd48ecbd0845c6
Sisun Cheng <997723678@qq.com>
2022-04-15 00:47:23 -0400

    finish update map function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,84 @@
 let map = L.map('map').setView([37.7707, -122.4378], 12);
+const parkingLayer = L.layerGroup().addTo(map);
 
 L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
 }).addTo(map);
+
+var slider = document.getElementById("mySlider");
+//var output = document.getElementById("demo");
+//output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+/*
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+*/
+let parkings = parkings_nhood_0327_0402;
+
+slider.addEventListener('change', () => {
+  console.log(slider.value);
+  updateMap();
+});
+
+let getColor = (value) => {
+  if (value === 0) {
+    return '#F8F8D4';
+  };
+  if (value > 0 && value <= 5) {
+    return '#ECF3BF';
+  };
+  if (value > 5 && value <= 10) {
+    return '#DCEFAB';
+  };
+  if (value > 10 && value <= 20) {
+    return '#C7EA97';
+  };
+  if (value > 20 && value <= 50) {
+    return '#AEE483';
+  };
+  if (value > 50 && value <= 100) {
+    return '#91DF70';
+  };
+
+  if (value > 100 && value <= 200) {
+    return '#71D85D';
+  };
+  if (value > 200 && value <= 500) {
+    return '#4ED24A';
+  };
+  if (value > 500 && value <= 1000) {
+    return '#38CB49';
+  };
+  if (value > 1000 && value <= 1500) {
+    return '#33BE54';
+  };
+  if (value > 1500 && value <= 2000) {
+    return '#2EB15D';
+  };
+  if (value > 2000 && value <= 4000) {
+    return '#29A363';
+  };
+  if (value > 4000 && value <= 5000) {
+    return '#249568';
+  };
+
+}
+
+
+let updateMap = () => {
+  parkingLayer.clearLayers();
+  L.geoJSON(parkings, {
+    style: feature => {
+      let col = feature.properties[slider.value];
+      return { color: getColor(col) };
+    },
+  })
+  .bindTooltip(layer => {
+    let nam = layer.feature.properties.nhood;
+    return `${nam}`;
+})
+  .addTo(parkingLayer);
+}
+//updateMap();
