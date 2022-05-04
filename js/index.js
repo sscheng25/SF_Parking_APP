9776e81d16e25fa4f2a1139ac6fbd034cb6a758e
Sisun Cheng <997723678@qq.com>
2022-05-04 16:51:30 -0400

    syntax update

--- js/index.js
+++ js/index.js
@@ -7,13 +7,13 @@ L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
 }).addTo(map);
 
-var slider = document.getElementById("mySlider");
-var output = document.getElementById("output-value");
-output.innerHTML = `Day ${Math.floor(slider.value/24)+1}, ${(slider.value)%24}:00`; // Display the default slider value
+let slider = document.getElementById('mySlider');
+let output = document.getElementById('output-value');
+output.innerHTML = `Day ${Math.floor(slider.value / 24) + 1}, ${(slider.value) % 24}:00`; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = `Day ${Math.floor(this.value/24)+1}, ${(this.value)%24}:00`;
-}
+  output.innerHTML = `Day ${Math.floor(this.value / 24) + 1}, ${(this.value) % 24}:00`;
+};
 
 let parkings = parkings_nhood_0327_0402;
 
@@ -26,45 +26,43 @@ slider.addEventListener('change', () => {
 let getColor = (value) => {
   if (value === 0) {
     return '#F8F8D4';
-  };
+  }
   if (value > 0 && value <= 5) {
     return '#ECF3BF';
-  };
+  }
   if (value > 5 && value <= 10) {
     return '#DCEFAB';
-  };
+  }
   if (value > 10 && value <= 20) {
     return '#C7EA97';
-  };
+  }
   if (value > 20 && value <= 50) {
     return '#AEE483';
-  };
+  }
   if (value > 50 && value <= 100) {
     return '#91DF70';
-  };
-
+  }
   if (value > 100 && value <= 200) {
     return '#71D85D';
-  };
+  }
   if (value > 200 && value <= 500) {
     return '#4ED24A';
-  };
+  }
   if (value > 500 && value <= 1000) {
     return '#38CB49';
-  };
+  }
   if (value > 1000 && value <= 1500) {
     return '#33BE54';
-  };
+  }
   if (value > 1500 && value <= 2000) {
     return '#2EB15D';
-  };
+  }
   if (value > 2000 && value <= 4000) {
     return '#29A363';
-  };
+  }
   if (value > 4000 && value <= 5000) {
     return '#249568';
-  };
-
+  }
 }
 
 
@@ -76,75 +74,69 @@ let updateMap = () => {
       return { color: getColor(col) };
     },
   })
-  .bindTooltip(layer => {
-    let nam = layer.feature.properties.nhood;
-    return `${nam}`;
-  })
-  .addTo(parkingLayer);
-}
+    .bindTooltip(layer => {
+      let nam = layer.feature.properties.nhood;
+      return `${nam}`;
+    })
+    .addTo(parkingLayer);
+};
 
 // barchart
 let initialBarChart = () => {
-  
   let nhood = parkings.features.map((layer) => {
-    let result = layer.properties.nhood; 
+    let result = layer.properties.nhood;
     return result;
   });
   let barValue = parkings.features.map((layer) => {
-    let result = layer.properties[slider.value]; 
+    let result = layer.properties[slider.value];
     return result;
   });
-  //console.log(barValue);
-  var options = {
+  // console.log(barValue);
+  let options = {
     chart: {
       type: 'bar',
       height: 250,
       events: {
         click: function(event, chartContext, config) {
-          //console.log(config.seriesIndex);
-          //console.log(config.config.series[config.seriesIndex]);
-          //console.log(config.dataPointIndex);
           console.log(parkings.features[config.dataPointIndex].properties.nhood);
-          //console.log('hey');
+          // console.log('hey');
           parkingLayer3.clearLayers();
           nameHood = parkings.features[config.dataPointIndex].properties.nhood;
           nhoodSelected = parkings.features.filter((layer) => layer.properties.nhood === nameHood);
           L.geoJSON(nhoodSelected, {
-            style: {color: '#0000FF'}
+            style: { color: '#0000FF' }
           })
-          .bindPopup(layer => {
-            let nam = nameHood;
-            return `${nam}`;
-          })
-          .openPopup()
-          .addTo(parkingLayer3);
-        }
-      }
+            .bindPopup(layer => {
+              let nam = nameHood;
+              return `${nam}`;
+            })
+            .openPopup()
+            .addTo(parkingLayer3);
+        },
+      },
     },
     series: [{
       name: 'parkings',
-      data: barValue
+      data: barValue,
     }],
     xaxis: {
       categories: nhood,
       labels: {
-        rotate: -90
+        rotate: -90,
       },
-      tickPlacement: 'on'
+      tickPlacement: 'on',
     },
-    
-  }
-  
-  var chart = new ApexCharts(document.querySelector("#bar-chart"), options);
+  };
+  let chart = new ApexCharts(document.querySelector('#bar-chart'), options);
   
   chart.render();
   chart.updateSeries([{
-    data: barValue
-  }], 
-  //animate = true
+    data: barValue,
+  }],
+  // animate = true
   )
 };
-//initialBarChart();
+// initialBarChart();
 const nhoodSelect = document.querySelector('#nhood-select');
 
 let initialSelectList = () => {
@@ -157,7 +149,7 @@ let initialSelectList = () => {
     const nhood_name = nhood.properties.nhood;
     nhoodList = nhoodList.concat(nhood_name);
   });
-  */
+*/
   let nhoodUnique = [...new Set(nhoodList)].sort();
   nhoodUnique.forEach((nhood) => {
     nhoodSelect.appendChild(htmlToElement(`<option>${nhood}</option>`));
@@ -176,34 +168,34 @@ let initialLineChart = () => {
     };
     
   };
-  var options_line = {
+  let options_line = {
     series: [{
       name: "Parking Counts",
-      data: valueList
-  }],
-    chart: {
-    height: 300,
-    type: 'line',
-    zoom: {
-      enabled: true
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  stroke: {
-    curve: 'straight'
-  },
-  grid: {
-    row: {
-      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-      opacity: 0.5
+      data: valueList,
+    }],
+      chart: {
+      height: 300,
+      type: 'line',
+      zoom: {
+        enabled: true,
+      },
     },
-  },
-  xaxis: {
-    categories: timeList,
-    tickAmount: 10
-  }
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'straight',
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: timeList,
+      tickAmount: 10,
+    },
   };
   
   var chart_line = new ApexCharts(document.querySelector("#line-chart"), options_line);
@@ -211,7 +203,7 @@ let initialLineChart = () => {
   chart_line.updateSeries([{
     data: valueList
   }], 
-  //animate = true
+  // animate = true
   )
 };
 let updateMaker = () => {
@@ -219,15 +211,15 @@ let updateMaker = () => {
   parkingLayer2.clearLayers();
   nhoodSelected = parkings.features.filter((layer) => layer.properties.nhood === nhoodSelect.value);
   L.geoJSON(nhoodSelected, {
-    style: {color: '#FF0000'}
+    style: { color: '#FF0000' }
   })
-  .bindPopup(layer => {
-    let nam = nhoodSelect.value;
-    return `${nam}`;
-  })
-  .openPopup()
-  .addTo(parkingLayer2);
-}
+    .bindPopup(layer => {
+      let nam = nhoodSelect.value;
+      return `${nam}`;
+    })
+    .openPopup()
+    .addTo(parkingLayer2);
+};
 
 let initialPieChart = () => {
   let valueList = [];
@@ -235,41 +227,40 @@ let initialPieChart = () => {
   for(let i = 0; i < 140; i++) {
     if (timePeriod.nhood[i] === nhoodSelect.value) {
       valueList = valueList.concat(timePeriod.value[i]);
-      //console.log(timePeriod.value[i]);
+      // console.log(timePeriod.value[i]);
       timeList = timeList.concat(timePeriod.timePeriod[i]);
-    };  
-  };
+    }
+  }
   let timeUnique = [...new Set(timeList)].sort();
   console.log(valueList);
   console.log(timeList);
-  var options_pie = {
+  let options_pie = {
     series: valueList,
     chart: {
       height: 250,
       type: 'donut',
-  },
-  labels: timeUnique,
-  responsive: [{
-    breakpoint: 480,
-    options: {
-      chart: {
-        height: 50,
-        width: 50
+    },
+    labels: timeUnique,
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          height: 50,
+          width: 50,
+        },
+        legend: {
+          position: 'bottom',
+        },
       },
-      legend: {
-        position: 'bottom'
-      }
-    }
-  }]
+    }],
   };
 
-
-  var chart_pie = new ApexCharts(document.querySelector("#pie-chart"), options_pie);
+  let chart_pie = new ApexCharts(document.querySelector("#pie-chart"), options_pie);
   chart_pie.render();
   chart_pie.updateSeries(
-    valueList
-  )
-}
+    valueList,
+  );
+};
 
 let handleSelectChange = () => {
   initialLineChart();
--- js/parkingMap.js
+++ js/parkingMap.js
@@ -1,33 +1,28 @@
 let map = L.map('map').setView([37.7707, -122.4378], 13);
-const parkingLayer = L.layerGroup().addTo(map);
-const parkingLayer2 = L.layerGroup().addTo(map);
-const parkingLayer3 = L.layerGroup().addTo(map);
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
 }).addTo(map);
 
-let parkings = parkings_nhood_0327_0402;
-var parkIcon = L.icon({
+// let parkings = parkings_nhood_0327_0402;
+let parkIcon = L.icon({
   iconUrl: './image/logo3.png',
-  iconSize:     [25, 25], // size of the icon
-  //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-  //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  iconSize: [25, 25], // size of the icon
 });
 
 L.geoJSON(offStreet, {
-  pointToLayer: function(feature, latlng){
-    return L.marker(latlng, {icon: parkIcon});
-  }
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon: parkIcon });
+  },
 })
-.bindTooltip(layer => {
-  let add = layer.feature.properties.street_address;
-  let cap = parseInt(layer.feature.properties.capacity);
-  return `<p>ADDRESS: ${add}</p><p>CAPACITY: ${cap}</p>`;
-})
-.addTo(map);
+  .bindTooltip(layer => {
+    let add = layer.feature.properties.street_address;
+    let cap = parseInt(layer.feature.properties.capacity);
+    return `<p>ADDRESS: ${add}</p><p>CAPACITY: ${cap}</p>`;
+  })
+  .addTo(map);
 
-var searchBar = document.getElementById("search-bar");
+let searchBar = document.getElementById('search-bar');
 console.log(searchBar.value);
 
 // Geolocation Tracking Button
@@ -45,21 +40,12 @@ let trackingID = null;
   following their position. When the user clicks the tracking button again, we
   will STOP following their position.
 */
-const trackingButton = document.querySelector('#button');
-trackingButton.addEventListener('click', () => {
-  if (trackingID === null) {
-    startTracking();
-  } else {
-    stopTracking();
-  }
-});
-
 
 const startTracking = function () {
   // Start tracking the position.
   console.log('Starting to track position...');
   trackingID = navigator.geolocation.watchPosition(handlePositionUpdated);
-  //console.log(trackingID);
+  // console.log(trackingID);
 
   // Update the button text.
   trackingButton.innerHTML = 'Stop Tracking Me.';
@@ -79,6 +65,17 @@ const stopTracking = function () {
   trackingButton.innerHTML = 'Track Me!';
 };
 
+const trackingButton = document.querySelector('#button');
+trackingButton.addEventListener('click', () => {
+  if (trackingID === null) {
+    startTracking();
+  } else {
+    stopTracking();
+  }
+});
+
+
+
 const offList = document.querySelector('#off-list');
 const handlePositionUpdated = function (position) {
   // Move the position marker to the
@@ -86,31 +83,30 @@ const handlePositionUpdated = function (position) {
   let lat0 = position.coords.latitude;
   let long0 = position.coords.longitude;
   positionMarker.setLatLng(latlng);
-  //map.panTo(latlng);
+  // map.panTo(latlng);
 
   // Now that we know the user's position, update the marker style.
   positionMarker.setStyle(trackingStyle);
 
   console.log(position);
-  
+
   let disDict = {};
   // calculate the distance
   for (let i = 0; i < 45; i++) {
     let lat1 = offStreet.features[i].properties.main_entrance_lat;
     let long1 = offStreet.features[i].properties.main_entrance_long;
-    distance = getDistanceFromLatLonInKm(lat0, long0, lat1, long1);
+    let distance = getDistanceFromLatLonInKm(lat0, long0, lat1, long1);
     console.log(offStreet.features[i].properties.street_address);
     console.log(distance);
     disDict[offStreet.features[i].properties.street_address] = distance;
-
-  };
+  }
   console.log(disDict);
-  var sortDist = Object.keys(disDict).map(function(key) {
+  let sortDist = Object.keys(disDict).map(function (key) {
     return [key, disDict[key]];
   });
-  
+
   // Sort the array based on the second element
-  sortDist.sort(function(first, second) {
+  sortDist.sort(function (first, second) {
     return first[1] - second[1];
   });
 
@@ -119,78 +115,72 @@ const handlePositionUpdated = function (position) {
 
   offList.innerHTML = '';
   sortDist.forEach((item) => {
-    offList.appendChild(htmlToElement(`<li><b>Location:</b> ${item[0]}<br><b>Distance:</b> ${Math.round(item[1]*1000)} m</li>`));
+    offList.appendChild(htmlToElement(`<li><b>Location:</b> ${item[0]}<br><b>Distance:</b> ${Math.round(item[1] * 1000)} m</li>`));
   });
-
-};
-
-let getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d;
 };
 
 let  deg2rad = (deg) => {
-  return deg * (Math.PI/180)
+  return deg * (Math.PI / 180);
 };
+
+let getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+  let R = 6371; // Radius of the earth in km
+  let dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  let dLon = deg2rad(lon2 - lon1);
+  let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1))
+    * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let d = R * c; // Distance in km
+  return d;
+};
+
+
 
 let searchAddress = () => {
   const search = searchBar.value;
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${search}.json?access_token=pk.eyJ1Ijoic2lzdW5jIiwiYSI6ImNsMHZscjk3dDE4OTYzam54OTJ4bjh6N2kifQ.daoGo-tfen82e1nw3iSYew`
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${search}.json?access_token=pk.eyJ1Ijoic2lzdW5jIiwiYSI6ImNsMHZscjk3dDE4OTYzam54OTJ4bjh6N2kifQ.daoGo-tfen82e1nw3iSYew`;
   let searchCoor = '';
 
   fetch(url)
-  .then(resp => resp.json())
-  .then(searchLoc => {
-    searchCoor = searchLoc.features[0].center;
-    //console.log(searchCoor);
-    let latSearch0 = searchCoor[1];
-    let longSearch0 = searchCoor[0];
+    .then(resp => resp.json())
+    .then(searchLoc => {
+      searchCoor = searchLoc.features[0].center;
+      let latSearch0 = searchCoor[1];
+      let longSearch0 = searchCoor[0];
 
-    let disDict = {};
-    // calculate the distance
-    for (let i = 0; i < 45; i++) {
-      let lat1 = offStreet.features[i].properties.main_entrance_lat;
-      let long1 = offStreet.features[i].properties.main_entrance_long;
-      distance = getDistanceFromLatLonInKm(latSearch0, longSearch0, lat1, long1);
-      console.log(offStreet.features[i].properties.street_address);
-      console.log(distance);
-      disDict[offStreet.features[i].properties.street_address] = distance;
+      let disDict = {};
+      // calculate the distance
+      for (let i = 0; i < 45; i++) {
+        let lat1 = offStreet.features[i].properties.main_entrance_lat;
+        let long1 = offStreet.features[i].properties.main_entrance_long;
+        let distance = getDistanceFromLatLonInKm(latSearch0, longSearch0, lat1, long1);
+        console.log(offStreet.features[i].properties.street_address);
+        console.log(distance);
+        disDict[offStreet.features[i].properties.street_address] = distance;
+      }
+      console.log(disDict);
+      let sortDist = Object.keys(disDict).map(function(key) {
+        return [key, disDict[key]];
+      });
+      // Sort the array based on the second element
+      sortDist.sort(function(first, second) {
+        return first[1] - second[1];
+      });
 
-    };
-    console.log(disDict);
-    var sortDist = Object.keys(disDict).map(function(key) {
-      return [key, disDict[key]];
-    });
-    
-    // Sort the array based on the second element
-    sortDist.sort(function(first, second) {
-      return first[1] - second[1];
-    });
+      console.log(sortDist);
+      sortDist = sortDist.slice(0, 10);
 
-    console.log(sortDist);
-    sortDist = sortDist.slice(0, 10);
-
-    offList.innerHTML = '';
-    sortDist.forEach((item) => {
-      offList.appendChild(htmlToElement(`<li><b>Location:</b> ${item[0]}<br><b>Distance:</b> ${Math.round(item[1]*1000)} m</li>`));
+      offList.innerHTML = '';
+      sortDist.forEach((item) => {
+        offList.appendChild(htmlToElement(`<li><b>Location:</b> ${item[0]}<br><b>Distance:</b> ${Math.round(item[1] * 1000)} m</li>`));
+      });
     });
-    });
-    
-}
+};
 
 const searchButton = document.querySelector('#button-search');
 searchButton.addEventListener('click', () => {
-  console.log("The term searched for was " + searchBar.value);
+  console.log('The term searched for was ' + searchBar.value);
   searchAddress();
- })
+});
 
 
